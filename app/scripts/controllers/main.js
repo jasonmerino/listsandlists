5c39999ac9bfbbe006ec671881fe06253352b99d
Jason Merino <jasonmerino@gmail.com>
2013-05-17 13:57:26 -0700

    added basic styles, responsive stuff, a couple click handlers

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,30 @@
 
 angular.module('listsandlistsApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    $scope.lists = [
+      {
+        item: 'Home Depot',
+        status: null,
+        total: 13
+      },
+      {
+        item: 'Food Maxx',
+        status: null,
+        total: 7
+      },
+      {
+        item: 'Raley\'s',
+        status: null,
+        total: 10
+      }
     ];
+    $scope.getListCount = function () {
+      return $scope.lists.length;
+    };
+    $scope.navigateToList = function () {
+      alert('nav to ' + this.list.item);
+    };
+    $scope.addList = function () {
+      alert('add list');
+    };
   }]);
