a8147922c0bf1605d7096409e53461bac3a058bd
Jason Merino <jasonmerino@gmail.com>
2013-05-27 09:51:35 -0700

    added id to each list item. added incrementing of ids to lists.

--- app/scripts/controllers/list.js
+++ app/scripts/controllers/list.js
@@ -8,10 +8,13 @@ angular.module('listsandlistsApp')
         // grab data from localStorage
         // TODO: write a better way to do this
         if ($window.localStorage.listData !== undefined) {
-          $scope.lists = JSON.parse($window.localStorage.listData);
+          $scope.data = JSON.parse($window.localStorage.listData);
         }
         else {
-          $scope.lists = [];
+          $scope.data = {
+            lists: [],
+            lastListId: -1
+          };
         }
 
         // store UI state within $scope
@@ -22,23 +25,38 @@ angular.module('listsandlistsApp')
         // init scope variables
         $scope.newItem = '';
 
-        // helper to get the current list for simpler access
+        /**
+         * Helper function to get the current list for simpler access
+         * @param  {Number} id
+         * @return {Object}
+         */
         function getListById(id) {
-          for (var i = 0; i < $scope.lists.length; i++) {
-            if ($scope.lists[i].id === id) {
-              return $scope.lists[i];
+          for (var i = 0; i < $scope.data.lists.length; i++) {
+            if ($scope.data.lists[i].id === id) {
+              return $scope.data.lists[i];
             }
           }
         }
+
+        /**
+         * Persist the JS data to localStorage
+         */
+        var persist = function () {
+          $window.localStorage.listData = JSON.stringify($scope.data);
+        };
 
         /**
          * Add the typed item to the list and persist it in localStorage
          */
         $scope.addItem = function () {
           if ($scope.newItem !== '') {
-            $scope.list.itemList.push($scope.newItem);
-            // persist
-            $window.localStorage.listData = JSON.stringify($scope.lists);
+            var item = {
+              id: $scope.list.lastItemId + 1,
+              item: $scope.newItem
+            };
+            $scope.list.lastItemId++;
+            $scope.list.itemList.push(item);
+            persist();
             // adjust UI
             $scope.ui.showAddItem = false;
             $scope.newItem = '';
@@ -52,7 +70,7 @@ angular.module('listsandlistsApp')
           $scope.ui.showAddItem = true;
           setTimeout(function () {
             document.getElementById('new-item').focus();
-          }, 0);
+          }, 10);
         };
 
         /**
@@ -62,6 +80,20 @@ angular.module('listsandlistsApp')
           $location.path('/');
         };
 
+        /**
+         * Remove the clicked item from the current list
+         */
+        $scope.removeItem = function () {
+          for (var i = 0; i < $scope.list.itemList.length; i++) {
+            if ($scope.list.itemList[i].id === this.listItem.id) {
+              // remove the item from the listItem array
+              $scope.list.itemList.splice(i, 1);
+            }
+          }
+          persist();
+        };
+
+        // create scoped pointer to the active list
         $scope.list = getListById(parseInt($routeParams.id, 10));
       }
     ]
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,39 +2,53 @@
 
 angular.module('listsandlistsApp')
   .controller('MainCtrl', ['$window', '$scope', '$location', function ($window, $scope, $location) {
-    $scope.lists = $window.localStorage.listData !== undefined ? JSON.parse($window.localStorage.listData) : [];
+
+    // grab data from localStorage
+    // TODO: write a better way to do this
+    if ($window.localStorage.listData !== undefined) {
+      $scope.data = JSON.parse($window.localStorage.listData);
+    }
+    else {
+      $scope.data = {
+        lists: [],
+        lastListId: -1
+      };
+    }
 
     $scope.showAddListUI = false;
     $scope.newList = '';
 
+    var persist = function () {
+      $window.localStorage.listData = JSON.stringify($scope.data);
+    };
+
     $scope.addList = function () {
       if ($scope.newList !== '') {
         // store in JS
-        $scope.lists.push({
-          id: 3,
+
+        $scope.data.lists.push({
+          id: $scope.data.lastListId + 1,
           item: $scope.newList,
+          lastItemId: -1,
           itemList: [],
           status: null,
           total: 0
         });
-        // persist
-        $window.localStorage.listData = JSON.stringify($scope.lists);
+        $scope.data.lastListId++;
+        persist();
         $scope.newList = '';
         $scope.showAddListUI = false;
       }
     };
 
-    $scope.getListCount = function () {
-      return $scope.lists.length;
-    };
     $scope.navigateToList = function () {
       $location.path('/list/' + this.list.id);
     };
+
     $scope.showAddList = function () {
       $scope.showAddListUI = true;
       setTimeout(function () {
         document.getElementById('list-to-add').focus();
-      }, 0);
-      //alert('add list');
+      }, 10);
     };
   }]);
